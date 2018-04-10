23fd800723db814f1dd56dc2b2b60bf027668d43
PC-THOMAS\Thomas <thomas.flamant96@gmail.com>
2018-04-10 15:37:49 +0200

    plus de php

--- includes/pdo.php
+++
@@ -1,9 +0,0 @@
-<?php
-const DATABASE_HOST = "localhost";
-const DATABASE_NAME = 'soireejeux';
-const DATABASE_USER = 'root';
-const DATABASE_PASSWORD = 'root';
-
-$pdo = new PDO('mysql:host='.DATABASE_HOST.'; dbname='.DATABASE_NAME,
-    DATABASE_USER, DATABASE_PASSWORD);
-
